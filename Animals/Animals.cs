23260d0f98f92185b0f780fc3adab18a47c4288c
Иван Момот <momotis2007@gmail.com>
2023-01-30 14:58:25 +0200

    Перероблено по 30.01

--- Animals/Animals.cs
+++ Animals/Animals.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace Animals
 {
-    public abstract class Animals
+    public abstract class Animals: IComparable<Animals>
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public double Mass { get; set; }
         public double Distance { get; set; }
+
+        public int CompareTo(Animals? other)
+        {
+            if (other == null) return 1;
+            int res = Food().Item2.CompareTo(other.Food().Item2);
+            return res == 0 ?  Name.CompareTo(other.Name) : res;
+        }
+
         public abstract (TypeOfFood, double) Food();
     }
 }
--- Animals/Program.cs
+++ Animals/Program.cs
@@ -9,37 +9,78 @@ namespace Animals
     {
         static void Main(string[] args)
         {
+
             List<Animals> animals = new List<Animals>()
             {
-                new Predator(){ ID = 1,Name = "Wolf", Distance = 200, Mass = 20},
-                new Predator(){ ID = 419, Name = "Raccoon", Distance = 20, Mass = 50},
-                new Herbivorous(){ID = 2348, Name = "Cow", Distance = 150, Mass = 250},
-                new Omnivore(){ID = 17638, Name = "Bear", Distance = 30, Mass = 500}
+                new Predator(){ID = 156, Name = "Wolf", Distance = 200, Mass = 20},
+                new Predator(){ID = 297, Name = "Raccoon", Distance = 20, Mass = 50},
+                new Herbivorous(){ID = 357, Name = "Cow", Distance = 150, Mass = 250},
+                new Omnivore(){ID = 976, Name = "Bear", Distance = 30, Mass = 500}
             };
-            //a - не виходить
-            var selectedList = from u in animals orderby u descending select u;
+            string nameFile = "input.txt";
+            // A
+            animals.Sort();
+
             foreach (var animal in animals)
             {
                 var food = animal.Food();
-                Console.WriteLine($"{animal.ID} {animal.Name}, Eats {food.Item2} kg of {food.Item1}");
+                Console.WriteLine($"{animal.Name}, Eats {food.Item2} kg of {food.Item1}");
+            }
+            Console.WriteLine(" ");
+            // B
+            foreach (var a in animals.Take(3))
+            {
 
+                Console.WriteLine(a.Name);
             }
-            Console.WriteLine("   ");
-            //b
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine(" ");
+            // C
+            foreach (var a in animals.TakeLast(3))
             {
-                Console.WriteLine($"{animals[i].Name}");
+                Console.WriteLine(a.ID);
             }
-            Console.WriteLine("   ");
-            //c
-            for (int i = 0; i < 3; i++)
+            Console.WriteLine(" ");
+            // D
+            ReadInfo(nameFile);
+        }
+
+        public static void ReadInfo(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+            List<Animals> animals = new List<Animals>();
+            while (true)
             {
-                Console.WriteLine($"Id: {animals[animals.Count - i].ID}");
+                string? line = sr.ReadLine();
+                if (line == null) break;
+                string[] N = line.Split(' ');
+
+                try
+                {
+                    Animals animal;
+
+                    if (N[3] == "Predator") animal = new Predator();
+                    else if (N[3] == "Herbivorous") animal = new Herbivorous();
+                    else if (N[3] == "Omnivore") animal = new Omnivore();
+                    else
+                    {
+                        Console.WriteLine("Reading error");
+                        continue;
+                    }
+
+                    animal.ID = Convert.ToInt32(N[0]);
+                    animal.Name = N[1];
+                    animal.Mass = Convert.ToDouble(N[2]);
+                    animal.Distance = Convert.ToDouble(N[3]);
+
+                    animals.Add(animal);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Reading error");
+                    continue;
+                }
             }
-            //d
-            StreamReader sr = new StreamReader("input.txt");
-            string[] s = sr.ReadLine().Split(',');
             sr.Close();
         }
-    }
+    } 
 }
